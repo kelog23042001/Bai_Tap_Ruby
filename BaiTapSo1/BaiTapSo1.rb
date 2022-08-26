98827e45a5e35dbdde28301a4b23051636e8dce1
Long Nguyen <kelong23042001@gmail.com>
2022-08-26 23:34:03 +0700

    first commit

---
+++ BaiTapSo1/BaiTapSo1.rb
@@ -0,0 +1,14 @@
+puts "Xin chào - Nguyễn Hoàng Kế Long - 1911505310232"
+
+print "Nhập số thứ 1: "
+sothu1 = gets
+print "Nhập số thứ 2: "
+sothu2 = gets
+
+puts "Số thứ 1: #{sothu1}Số thứ 2: #{sothu2}"
+
+if sothu1 > sothu2
+    puts "Giá trị lớn nhất: #{sothu1}"
+else
+    puts "Giá trị lớn nhất: #{sothu2}"
+end

e3a5bcac62e50bb5770d5273e1b4564c4040add1
Long Nguyen <kelong23042001@gmail.com>
2022-08-28 09:35:11 +0700

    thêm nội dung đề bài

--- BaiTapSo1/BaiTapSo1.rb
+++ BaiTapSo1/BaiTapSo1.rb
@@ -1,5 +1,13 @@
+#Viết lệnh hiển thị xin chào + tên SV + mã SV
 puts "Xin chào - Nguyễn Hoàng Kế Long - 1911505310232"
 
+#Nhập 1 số, in ra số đó
+print "Nhập một số: "
+so = gets
+puts "Số đã nhập: #{so} "
+
+#So sánh 2 số nhập từ bàn phím, in ra số lớn nhất
+puts "So sánh 2 số"
 print "Nhập số thứ 1: "
 sothu1 = gets
 print "Nhập số thứ 2: "
